7dcb45a56a18fa60c3af11f790e0d2f1889e24f8
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-04-13 23:57:30 +0530

    transpose of matrix

---
+++ video 25/q25.js
@@ -0,0 +1,19 @@
+// Examples 
+// [ [3,4, 8],
+// [5,6,9]]
+// Transponse:-
+    // [[3,5],
+    //  [4,6],
+    //  [8,9]]
+//  Javascript
+
+function transpose(){
+    arr1 = [[3,4,8], [5,6,9]]
+
+    for(let i=0; i<=arr1.length; i++){
+        console.log(arr1[i]);   
+        
+    }
+}
+
+transpose()
