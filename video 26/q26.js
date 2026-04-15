299c7df067fb2adb2c20fe8fe6ba00f393095fb4
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-04-15 23:59:47 +0530

    q26 perfect number

---
+++ video 26/q26.js
@@ -0,0 +1,7 @@
+// Write a program that checks if a given number is a perfect number
+// Perfect number : The sum of its proper divisors equals the number itself.
+// ex- 1+2+3= 6 = true
+
+function checkPerfectNum(number){
+    
+}
