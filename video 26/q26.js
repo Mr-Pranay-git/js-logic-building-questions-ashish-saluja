6fb82385f6f1b81307af6169a0b6c248590e84b8
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-04-16 23:59:12 +0530

    perfect number completed

--- video 26/q26.js
+++ video 26/q26.js
@@ -3,5 +3,19 @@
 // ex- 1+2+3= 6 = true
 
 function checkPerfectNum(number){
-    
+    const sum = 0;
+    const properDivisor = []; 
+
+    for(let i =1; i<number; i++){  
+        if(number%i === 0){  
+            properDivisor.push(i)        
+            sum += i
+        }
+        else {
+            continue;
+        }
+    }
+    return sum == number ? `${number} perfect number`: `${number} is not a perfect number`;
 }
+
+console.log(checkPerfectNum(28))
