2ac65fcd5addc8c878c0b2ecf40b71e15d171afd
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-04-19 23:58:54 +0530

    strong password que

--- video 27/q27.js
+++ video 27/q27.js
@@ -1,7 +1,11 @@
 // Write a program that checks if the provided password is
+<<<<<<< HEAD
 // strong or not
 
 function password(password) {
     
 
 }
+=======
+// strong or not
+>>>>>>> 74de72c (strong password que)
